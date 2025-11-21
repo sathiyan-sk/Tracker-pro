c2160b9ca884a03caeda552e7873ce4823e0ed2f
sathiyan-sk <workspace4sathiyan@gmail.com>
2025-11-21 12:10:52 +0530

    Project Initial for Start

---
+++ src/test/java/com/webapp/Tracker_pro/TrackerProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.Tracker_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
